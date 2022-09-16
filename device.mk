c027e93b02d543a0cc67023786e144c3e2472e4b
bengris32 <bengris32@protonmail.ch>
2022-09-16 21:53:03 +0100

    CK8n: Build android.hidl.allocator@1.0
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -162,7 +162,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.allocator@1.0 \
     android.hidl.base@1.0.vendor \
+    android.hidl.allocator@1.0.vendor \
     libhidltransport.vendor \
     libhwbinder.vendor
 
